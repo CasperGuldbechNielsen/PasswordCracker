04bb27afa8878d5ba5f187635ba0fab4653def31
Casper Nielsen <Casp674d@edu.easj.dk>
2017-03-02 11:05:16 +0100

    Added lifecycle of single threaded Master

--- PasswordCrackerMaster/Program.cs
+++ PasswordCrackerMaster/Program.cs
@@ -17,6 +17,13 @@ namespace PasswordCrackerMaster
         private static int _dictDivide;
         private static string _message;
         private static string _stopMessage;
+        private static List<string> _slaveList;
+        private static List<string> _slaveListToSend;
+        private static int _listNum;
+        private static string _jsonMessage;
+        private static Dictionary<int, Dictionary<string, string>> _mainDict;
+        private static Dictionary<string, string> _nestedDict;
+
 
         static void Main(string[] args)
         {
@@ -41,6 +48,9 @@ namespace PasswordCrackerMaster
                 StreamReader streamReader = new StreamReader(stream);
                 StreamWriter streamWriter = new StreamWriter(stream) { AutoFlush = true };
 
+                _slaveList = new List<string>();
+                _slaveListToSend = new List<string>();
+
                 // Read input
 
                 _message = streamReader.ReadLine();
@@ -50,17 +60,105 @@ namespace PasswordCrackerMaster
                 if (!string.IsNullOrEmpty(_message))
                 {
                     // Deserialize the message
-                    var jsonMessage = JsonConvert.DeserializeObject(_message);
-                    
-                    // Get slave IP and such..
-                    Console.WriteLine("Client wrote: " + _message);
+                    _jsonMessage = Convert.ToString(JsonConvert.DeserializeObject(_message));
                 }
+                else
+                {
+                    // do something
+                }
+
+                string[] slaveInfo = _jsonMessage.ToString().Split(',');
+
+                string[] slaveName = slaveInfo[0].Split(':');
+                string[] slaveIp = slaveInfo[1].Split(':');
+
+                // Get slave IP and such..
+                Console.WriteLine($"Slave connected with name {slaveName[1]} nd IP {slaveIp[1]}.");
 
                 Console.WriteLine("Sending work to slave...");
 
-                //
-                // Send work
-                //
+                foreach(var item in Resource.webster_dictionary_reduced)
+                {
+                    _slaveList.Add(Convert.ToString(item));
+                }
+
+                var listLenght = _slaveList.Count();
+                listLenght = listLenght / _dictDivide;
+
+                Console.WriteLine($"Sending {listLenght} lines to slave");
+
+                for(int i = 0; i <= listLenght; i++)
+                {
+                    _slaveListToSend.Add(_slaveList[i]);
+                }
+
+                var jsonSendList = JsonConvert.SerializeObject(_slaveListToSend);
+
+                // Send data
+                streamWriter.Write(jsonSendList);
+
+                // Add work to dict
+                _nestedDict.Add(slaveName[1], "sent");
+                _mainDict.Add(_listNum, _nestedDict);
+
+                // Wait for slave to respond.
+
+                var slaveResponse = streamReader.ReadLine();
+
+                if (!string.IsNullOrEmpty(slaveResponse))
+                {
+                    // Deserialize the message
+                    var response = Convert.ToString(JsonConvert.DeserializeObject(slaveResponse));
+
+                    string[] responseArr = _jsonMessage.ToString().Split(',');
+
+                    string[] slaveNameArr = slaveInfo[0].Split(':');
+                    string[] slaveResponseArr = slaveInfo[1].Split(':');
+
+                    Console.WriteLine($"Slave: {slaveNameArr[1]} responded: {slaveResponseArr}");
+
+                    _nestedDict[slaveNameArr[1]] = slaveResponseArr[1];
+                }
+                else
+                {
+                    // do something
+                }
+
+                while (true)
+                {
+                    // Wait for slave to finish
+
+                    var slaveNewResponse = streamReader.ReadLine();
+
+                    if (!string.IsNullOrEmpty(slaveNewResponse))
+                    {
+                        // Deserialize the message
+                        var response = Convert.ToString(JsonConvert.DeserializeObject(slaveNewResponse));
+
+                        string[] responseArr = _jsonMessage.ToString().Split(',');
+
+                        string[] slaveNameArr = slaveInfo[0].Split(':');
+                        string[] slaveResponseArr = slaveInfo[1].Split(':');
+
+                        Console.WriteLine($"Slave: {slaveNameArr[1]} responded: {slaveResponseArr}");
+
+                        _nestedDict[slaveNameArr[1]] = slaveResponseArr[1];
+
+                        if (slaveResponseArr[1] == "Done")
+                        {
+                            Console.WriteLine($"Slave: {slaveNameArr[1]} is done with his work");
+                            break;
+                        }
+                        else
+                        {
+                            // Just continue..
+                        }
+                    }
+                    else
+                    {
+                        // do something
+                    }              
+                }
 
                 // Stopping the server
 
